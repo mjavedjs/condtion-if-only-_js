25e281916870e8f57178ef20ed7d2284e2138853
muhammadjaved238 <mjavedjs2006@gmail.com>
2024-08-22 13:29:56 +0500

    if Condtion

---
+++ app.js
@@ -0,0 +1,11 @@
+var movie_day=prompt("Enter day For Movie");
+if(movie_day){
+ console.log(movie_day)
+ if(movie_day);
+}
+ if(movie_day=="sunday"){
+    console.log("the movie day")
+}
+if(movie_day!="sunday"){
+    console.log("stay conncted Not a movie day")
+}
